d88ac7aa760ee492a43ac48d8835fb774b24fd80
akilaTao <taosixu@umich.edu>
2018-02-18 18:01:51 -0500

    Client infrastructure added

--- client.py
+++ client.py
@@ -6,28 +6,36 @@ import click
 import socket
 
 
-def send_client_request(client_id, my_request_list, replica_config):
+def send_client_request(my_id, my_ip, my_port, my_request_list, replica_config):
     # { request_no : message }
     my_request_list = my_request_list
     # { replica_id : { 'ip' : '', 'port' : '' } }
     replica_config = replica_config
 
+    # Build the socket to receive external messages
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.bind((my_ip, my_port))
+    my_socket.listen(5)
+
     for request_no, value in my_request_list.items():
         message = {
             'message_type' : 'client_request',
-            'client_id' : client_id,
+            'client_id' : my_id,
             'client_request_no' : request_no,
             'value' : value
         }
 
         data = json.dumps(message)
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(('localhost', 6000))
-        client_socket.sendall(str.encode(data))
-        client_socket.close()
+        sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sender_socket.connect(('localhost', 6000))
+        sender_socket.sendall(str.encode(data))
+        sender_socket.close()
 
         time.sleep(1)
+
+    my_socket.close()
 
 
 @click.command()
@@ -35,7 +43,7 @@ def send_client_request(client_id, my_request_list, replica_config):
 @click.argument('client_request_file')
 @click.argument('replica_config_file')
 def main(client_id, client_request_file, replica_config_file):
-    client_id = int(client_id)
+    my_id = int(client_id)
     # Read necessary information from config_file
     client_request_file_handle = open(client_request_file, 'r')
     client_request_data = client_request_file_handle.read()
@@ -47,7 +55,7 @@ def main(client_id, client_request_file, replica_config_file):
     replica_config_list = json.loads(replica_config_data).get('replica_list')
 
     # { request_no : message }
-    my_request_to_send = json.loads(client_request_data).get(str(client_id))
+    my_request_to_send = json.loads(client_request_data).get(str(my_id))
     my_request_list = {}
     for request_no, value in my_request_to_send.items():
         my_request_list[int(request_no)] = value
@@ -60,9 +68,10 @@ def main(client_id, client_request_file, replica_config_file):
             'port' : raw_config['port']
         }
 
-    del replica_config[client_id]
+    my_ip = 'localhost'
+    my_port = 8000
 
-    send_client_request(client_id, my_request_list, replica_config) 
+    send_client_request(my_id, my_ip, my_port, my_request_list, replica_config) 
 
 
 if __name__ == '__main__':
