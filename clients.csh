f71b9ff3ec42647c14ff25413d6c92c4733d7fcb
akilaTao <taosixu@umich.edu>
2018-02-25 15:54:14 -0500

    generate_clients_csh.py

--- clients.csh
+++ clients.csh
@@ -1,4 +1,1 @@
-python client.py 0 localhost 8000 replica_config.json < messages.txt &
-python client.py 1 localhost 8001 replica_config.json < messages.txt &
-python client.py 2 localhost 8002 replica_config.json < messages.txt &
-python client.py 3 localhost 8003 replica_config.json < messages.txt &
+python client.py 0 localhost 8000 replica_config.json 30 < /Users/taosixu/Desktop/P1/msg/messages_0.txt &
--- generate_client_csh.py
+++ generate_client_csh.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import click
+import shutil
 
 
 @click.command()
@@ -8,17 +10,28 @@ import click
 @click.argument('msg_no')
 @click.argument('client_drop_rate')
 def main(client_no, msg_no, client_drop_rate):
-    shell_file = open('clients.csh','w')
+
+    base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'msg')
+    if os.path.isdir(base_dir):
+        shutil.rmtree(base_dir)
+    os.makedirs(base_dir)
+
+    shell_file = open('clients.csh', 'w')
 
     for client_i in range(int(client_no)):
-    	shell_file.write('python client.py {} localhost {} replica_config.json {} < messages_{}.txt &\n'.\
-    					 format(client_i, 8000+client_i, client_drop_rate, client_i))
+        msg_filename = os.path.join(base_dir, 'messages_{}.txt'.format(client_i))
 
-    	messagetxt = open('message_{}.txt'.format(client_i),'w')
+        shell_file.write('python client.py {} localhost {} replica_config.json {} < {} &\n'.\
+                         format(client_i, 8000+client_i, client_drop_rate, msg_filename))
 
-    	for i in range(int(msg_no)):
-    		messagetxt.write('s\n')
-    		messagetxt.write('message_{}_{}\n'.format(client_i, i))
+        messagetxt = open(msg_filename, 'w')
+
+        for i in range(int(msg_no)):
+            messagetxt.write('s\n')
+            messagetxt.write('message_{}_{}\n'.format(client_i, i))
+
+        messagetxt.write('p\n')
+        messagetxt.write('e\n')
 
     shell_file.close()
 
